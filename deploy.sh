5e3328b19c93a35aa61689112a702e87f20d7198
YannCharbon <yann.charbon@ik.me>
2023-04-05 14:13:31 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ git checkout 2023.02-rc2
 echo "Applying custom RPI CM4 defconfig"
 cp ../configs/buildroot-2023-rc2.config .config
 echo "Patching Buildroot"
-cp -r ../patches/buildroot-2023-rc2/ .
+cp -rf ../patches/buildroot-2023-rc2/* .
 echo "Creating deploy directories"
 sudo mkdir -p /srv/tftp/
 sudo mkdir -p /srv/nfs/
 sudo chown -R $USER /srv/
+sudo chmod 777 -R /srv/
 echo "Deploying Buildroot"
 make -j8
 echo "Initializing git in Linux and U-Boot for convenience"
